a104a227c89a95cc03315fa8af8f96c0bf124323
Raza Ali <smabidi32@gmail.com>
2023-06-27 01:49:46 +0500

    Fixed Compiler Run error

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "C:/Users/Abidi/Desktop/C-Library-GPS-Parsing-NMEA-ESP-IDF-v5/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/Users/Abidi/esp/esp-idf/.git")
+set(GIT_DIR "C:/Users/Abidi/Desktop/C-Library-GPS-Parsing-NMEA-ESP-IDF-v5/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
--- build/esp-idf/cmake_install.cmake
+++ build/esp-idf/cmake_install.cmake
@@ -392,3 +392,8 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("C:/Users/Abidi/Desktop/C-Library-GPS-Parsing-NMEA-ESP-IDF-v5/build/esp-idf/main/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/Abidi/Desktop/C-Library-GPS-Parsing-NMEA-ESP-IDF-v5/build/esp-idf/gps_parse/cmake_install.cmake")
+endif()
+
