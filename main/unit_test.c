1c568884857e7cd68149d516b002f6325df601f7
Raza Ali <smabidi32@gmail.com>
2023-07-10 06:41:55 +0500

    Debugged Unit Test

--- main/unit_test.c
+++ main/unit_test.c
@@ -4,17 +4,17 @@
 #include "gps_parser.h" // Include the header file of the function to be tested
 #include "esp_log.h"
 
-static const char *TAG = "";
+
 
 
 // Function to run the unit tests
-void runTests()
+void runTest()
 {
     // Test case 1: Valid GPS data
-    char payload[] = "$GPGGA,002153.000,3342.6618,N,11751.3858,W,1,10,1.2,27.0,M,-34.2,M,,0031*70";
+    char packet[] = "$GPGGA,002153.000,3342.6618,N,11751.3858,W,1,10,1.2,27.0,M,-34.2,M,,0031*5E";
     GpsData gpsdata;
-    data = parse_gps_data(packet, data);
-    if(data.checkpass)
+    gpsdata = parse_gps_data(packet, gpsdata);
+    if(gpsdata.checkpass)
     {
         ESP_LOGW(TAG, "Checksum is Validated.\n");
     }
@@ -151,7 +151,7 @@ void runTests()
         ESP_LOGE(TAG, "Test case 1: Failed (Units of Age Correction)\n");
     }
 
-    if (!(strcmp(gpsdata.calculated_checksum, "70")))
+    if (!(strcmp(gpsdata.calc_checksum, "5E")))
     {
         ESP_LOGD(TAG, "Test case 1: Passed (Calculated Checksum)\n");
     }
@@ -160,151 +160,8 @@ void runTests()
         ESP_LOGE(TAG, "Test case 1: Failed (Calculated Checksum)\n");
     }
 
-    // Test case 2: Empty payload
-    char emptyPayload[] = "";
-    gps_data emptyGpsData = Parse_gps_data(emptyPayload);
-
-    // Test individual struct fields for empty payload
-    if ((!strcmp(emptyGpsData.time_stamp, invalid)))
-    {
-        ESP_LOGD(TAG, "Test case 2: Passed (time_stamp)\n");
-    }
-    else
-    {
-        ESP_LOGE(TAG, "Test case 2: Failed (time_stamp)\n");
-    }
-
-        if (!(strcmp(emptyGpsData.latitude, invalid)))
-    {
-        ESP_LOGD(TAG, "Test case 2: Passed (latitude)\n");
-    }
-    else
-    {
-        ESP_LOGE(TAG, "Test case 2: Failed (latitude)\n");
-    }
-
-    if (!(strcmp(emptyGpsData.lat_direction, invalid)))
-    {
-        ESP_LOGD(TAG, "Test case 2: Passed (Latitude direction)\n");
-    }
-    else
-    {
-        ESP_LOGE(TAG, "Test case 2: Failed (Latitude direction)\n");
-    }
-
-    if (!(strcmp(emptyGpsData.longitude, invalid)))
-    {
-        ESP_LOGD(TAG, "Test case 2: Passed (Longitude)\n");
-    }
-    else
-    {
-        ESP_LOGE(TAG, "Test case 2: Failed (Longitude)\n");
-    }
-
-
-    if (!(strcmp(emptyGpsData.lon_direction, invalid)))
-    {
-        ESP_LOGD(TAG, "Test case 2: Passed (Longitude Direction)\n");
-    }
-    else
-    {
-        ESP_LOGE(TAG, "Test case 2: Failed (Longitude Direction)\n");
-    }
-
-    if (!(strcmp(emptyGpsData.qual_ind, invalid)))
-    {
-        ESP_LOGD(TAG, "Test case 2: Passed (Quality Indicator)\n");
-    }
-    else
-    {
-        ESP_LOGE(TAG, "Test case 2: Failed (Quality Indicator)\n");
-    }
-
-    if (!(strcmp(emptyGpsData.num_satellites, invalid)))
-    {
-        ESP_LOGD(TAG, "Test case 2: Passed (Num of Satellites)\n");
-    }
-    else
-    {
-        ESP_LOGE(TAG, "Test case 2: Failed (Num of Satellites)\n");
-    }
-
-    if (!(strcmp(emptyGpsData.HDOP, invalid)))
-    {
-        ESP_LOGD(TAG, "Test case 2: Passed (HDOP)\n");
-    }
-    else
-    {
-        ESP_LOGE(TAG, "Test case 2: Failed (HDOP)\n");
-    }
-
-    if (!(strcmp(emptyGpsData.altitude_antenna, invalid)))
-    {
-        ESP_LOGD(TAG, "Test case 2: Passed (Altitude of antenna)\n");
-    }
-    else
-    {
-        ESP_LOGE(TAG, "Test case 2: Failed (Altitude of antenna)\n");
-    }
-
-
-    if (!(strcmp(emptyGpsData.units_antenna, invalid)))
-    {
-        ESP_LOGD(TAG, "Test case 2: Passed (Units of Antenna)\n");
-    }
-    else
-    {
-        ESP_LOGE(TAG, "Test case 2: Failed (Units of Antenna)\n");
-    }
-
-
-    if (!(strcmp(emptyGpsData.geoidal_separation, invalid)))
-    {
-        ESP_LOGD(TAG, "Test case 2: Passed (Geoidal Separation)\n");
-    }
-    else
-    {
-        ESP_LOGE(TAG, "Test case 2: Failed (Geoidal Separation)\n");
-    }
-
-    if (!(strcmp(emptyGpsData.units_separation, invalid)))
-    {
-        ESP_LOGD(TAG, "Test case 2: Passed (Units of Geoidal Separation)\n");
-    }
-    else
-    {
-        ESP_LOGE(TAG, "Test case 2: Failed (Units of Geoidal Separation)\n");
-    }
-
-    if (!(strcmp(emptyGpsData.age_correction, invalid)))
-    {
-        ESP_LOGD(TAG, "Test case 2: Passed (Age Correction)\n");
-    }
-    else
-    {
-        ESP_LOGE(TAG, "Test case 2: Failed (Age Correction)\n");
-    }
-
-    if (!(strcmp(emptyGpsData.correction_sta, invalid)))
-    {
-        ESP_LOGD(TAG, "Test case 2: Passed (Units of Age Correction)\n");
-    }
-    else
-    {
-        ESP_LOGE(TAG, "Test case 2: Failed (Units of Age Correction)\n");
-    }
-
-    if (!(strcmp(emptyGpsData.calculated_checksum, invalid)))
-    {
-        ESP_LOGD(TAG, "Test case 2: Passed (Calculated Checksum)\n");
-    }
-    else
-    {
-        ESP_LOGE(TAG, "Test case 2: Failed (Calculated Checksum)\n");
-    }
 }
-
 void app_main(void)
 {
-    runTests(); // Run the unit tests
+    runTest(); // Run the unit tests
 }
